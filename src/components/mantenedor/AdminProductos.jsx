0e38bb43e2bbb3f4834e903dff81e76baeb61ce8
mgonzalen1979 <mauricio.esteban.1979@alumnos.cei.es>
2023-10-28 18:02:20 +0200

    permite subir imagen al crear producto

--- src/components/mantenedor/AdminProductos.jsx
+++ src/components/mantenedor/AdminProductos.jsx
@@ -8,99 +8,97 @@ import { Context } from "../contexto/Context";
 import { useContext } from "react";
 
 function AdminProductos() {
-  
   const [visibleCrear, setVisibleCrear] = useState(false);
   const [formNombre, setFormNombre] = useState("");
   const [formDescripcion, setFormDescripcion] = useState("");
   const [formPrecio, setFormPrecio] = useState("");
-  const [formTipoProducto, setFormTipoProducto] = useState('');
-  const { tiposProducto, listado, setListado}=useContext(Context);
+  const [formTipoProducto, setFormTipoProducto] = useState("");
+  const { tiposProducto, listado, setListado } = useContext(Context);
 
   const navigate = useNavigate();
 
+  const [imagen,setImagen] = useState();
+  const [imagenUrl, setImagenUrl] = useState();
+
+  function handleImagenCambio(e)
+  {
+      setImagen(e.target.files[0])
+  }
+
   function handleCrear(e) {
     e.preventDefault();
-  
+
     const nombre = formNombre;
     const tipo = formTipoProducto;
     const descripcion = formDescripcion;
-    const precio = formPrecio; 
-   
+    const precio = formPrecio;
 
-    if(validaPrecio(precio) && validaTipo(tipo))
-    {
-        //invoka api crear producto
-        axios.post("http://localhost:3000/api/productos/producto",
-        {
-            nombre:nombre,
-            descripcion:descripcion,
-            tipoProducto:tipo,
-            precio:precio,
-            descuento:0
-        }).
-        then((data)=>{
-            console.log("llegó al request!");
-          
+    const formData = new FormData();
+    formData.append('imagen', imagen);
+    formData.append("nombre", nombre); 
+    formData.append("descripcion", descripcion);
+    formData.append("tipoProducto", tipo);
+    formData.append("precio", precio);
+    formData.append("descuento", 0);
 
-            if(data.data.status==0){               
-                alert("producto creado correctamente");
-                //navigate('/adminProductos');
 
-                const nuevo ={ 
-                  ID:data.data.idPedido,
-                  Nombre:nombre,
-                  Descripcion:descripcion,
-                  urlFoto:'',
-                  tipoProducto:tipo,
-                  precio:precio,
-                  descuento:0                  
-              };
-                setListado(old=>[...old, nuevo]);
-       
-                setFormNombre('');
-                setFormDescripcion('');
-                setFormPrecio(0);
-                setFormTipoProducto('');
-                setVisibleCrear(!visibleCrear);
-                 
-            }
-            else{
-                alert("error al ingresar producto en base datos");
-            }
-        }).catch((error)=>{
-            console.log(error);
-            console.log("error al registrar producto");
+    if (validaPrecio(precio) && validaTipo(tipo)) {
+      //invoka api crear producto
+      axios
+        .post("http://localhost:3000/api/productos/producto", formData)
+        .then((data) => {
+          console.log("llegó al request!");
+
+          if (data.data.status == 0) {
+            alert("producto creado correctamente");
+            //navigate('/adminProductos');
+
+            const nuevo = {
+              ID: data.data.idPedido,
+              Nombre: nombre,
+              Descripcion: descripcion,
+              urlFoto: "",
+              tipoProducto: tipo,
+              precio: precio,
+              descuento: 0,
+            };
+            setListado((old) => [...old, nuevo]);
+
+            setFormNombre("");
+            setFormDescripcion("");
+            setFormPrecio(0);
+            setFormTipoProducto("");
+            setVisibleCrear(!visibleCrear);
+          } else {
+            alert("error al ingresar producto en base datos");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log("error al registrar producto");
         });
-
     }
   }
 
-  const validaPrecio =(precio)=>
-  {
-    const patternPrecio =/^[0-9]+([.][0-9]+)?$/;
-    if(patternPrecio.test(precio)){
-        console.log("precio valido "+precio);
-        return true;
-    }
-    else{ 
-        console.log("precio no valido "+precio);
-        return false;
-    }
-  }
-
-  const validaTipo =(tipo)=>
-  {
-    if(tipo.trim()=='') {
-      alert("debe seleccionar tipo de producto");
+  const validaPrecio = (precio) => {
+    const patternPrecio = /^[0-9]+([.][0-9]+)?$/;
+    if (patternPrecio.test(precio)) {
+      console.log("precio valido " + precio);
+      return true;
+    } else {
+      console.log("precio no valido " + precio);
       return false;
     }
-    else return true;
-  }
+  };
 
+  const validaTipo = (tipo) => {
+    if (tipo.trim() == "") {
+      alert("debe seleccionar tipo de producto");
+      return false;
+    } else return true;
+  };
 
-
-  useEffect(() => {   
-
+  useEffect(() => {
     axios
       .get("http://localhost:3000/api/productos/obtieneListaProductos")
       .then((result) => {
@@ -117,11 +115,9 @@ function AdminProductos() {
       });
   }, []);
 
-
-
-
   return (
-    <><Header />
+    <>
+      <Header />
       <div>
         <p>Lista de productos</p>
         <button
@@ -136,22 +132,44 @@ function AdminProductos() {
         <p>Formulario crear producto</p>
         <br />
         <form onSubmit={handleCrear}>
-        <label htmlFor="nombre">Nombre:</label>
-          <input type="text" id="nombre" 
-          onChange={(e)=>{setFormNombre(e.target.value)}}></input>
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            id="nombre"
+            onChange={(e) => {
+              setFormNombre(e.target.value);
+            }}
+          ></input>
           <br />
 
           <label htmlFor="descripcion">Descripcion:</label>
-          <input type="text" id="descripcion" onChange={(e)=>{setFormDescripcion(e.target.value)}}>
-           
-          </input>
+          <input
+            type="text"
+            id="descripcion"
+            onChange={(e) => {
+              setFormDescripcion(e.target.value);
+            }}
+          ></input>
           <br />
 
           <label htmlFor="precio">Precio:</label>
-          <input type="text" id="precio" onChange={(e)=>{setFormPrecio(e.target.value);validaPrecio(e.target.value)}}></input>
+          <input
+            type="text"
+            id="precio"
+            onChange={(e) => {
+              setFormPrecio(e.target.value);
+              validaPrecio(e.target.value);
+            }}
+          ></input>
           <br />
-        <label htmlFor="tipo">Tipo:</label>
-          <select id="tipoProducto " onChange={(e)=>{setFormTipoProducto(e.target.value);validaTipo(e.target.value)}}>
+          <label htmlFor="tipo">Tipo:</label>
+          <select
+            id="tipoProducto "
+            onChange={(e) => {
+              setFormTipoProducto(e.target.value);
+              validaTipo(e.target.value);
+            }}
+          >
             {tiposProducto.map((item) => {
               console.log(item);
               return (
@@ -162,28 +180,42 @@ function AdminProductos() {
             })}
           </select>
 
-          <input type="submit" value="Crear" />          
+          <label htmlFor="imagen">Imagen</label>
+          <input
+            type="file"
+            name="imagen"
+            id="iamgen"
+            accept="image/*"
+            onChange={handleImagenCambio}
+          />
+
+          <input type="submit" value="Crear" />
         </form>
-        <button onClick={() => {
+        <button
+          onClick={() => {
             setVisibleCrear(!visibleCrear);
-          }} >Volver</button>
+          }}
+        >
+          Volver
+        </button>
       </div>
-      {listado?<div hidden={visibleCrear}>
-        {listado.length == 0 ? (
-          <p>No hay productos</p>
-        ) : (
-          <ul>
-            {listado.map((producto) => {
-              return (
-                <li key={producto.ID}>
-                  <ProductoItemAdmin producto={producto} />
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-    :null}
+      {listado ? (
+        <div hidden={visibleCrear}>
+          {listado.length == 0 ? (
+            <p>No hay productos</p>
+          ) : (
+            <ul>
+              {listado.map((producto) => {
+                return (
+                  <li key={producto.ID}>
+                    <ProductoItemAdmin producto={producto} />
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+      ) : null}
     </>
   );
 }

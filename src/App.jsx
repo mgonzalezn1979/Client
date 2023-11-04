67c386db2d1abecb03eb44334a9b03b6a6c419af
mgonzalen1979 <mauricio.esteban.1979@alumnos.cei.es>
2023-11-04 20:09:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -59,9 +59,10 @@ function App() {
  /** se utiliza para mostar mensajes enn pantalla como resultado de alguan operacion exitosa o no */
   const [mensajeria, setMensajeria] = useState("");
   /** se agrega para definir el host de la api para modificar al realiza un deploy */
-  const [URL_PATH_API, setURL_PATH_API] = useState("http://localhost:3000");;//useState("http://localhost:3000");//useState("https://server-zi2v.onrender.com");
-//en prod https://server-zi2v.onrender.com
-//useState("http://localhost:3000");
+  const [URL_PATH_API, setURL_PATH_API] = useState("http://localhost:3000");//useState("http://localhost:3000");;//useState("http://localhost:3000");//useState("https://server-zi2v.onrender.com");
+
+//local:useState("http://localhost:3000");
+//en prod:useState("https://server-zi2v.onrender.com");
 
   /** Mantiene el calculo de los totales, subtotal y cantidad de productos 
    * al crear y modificar el pedido 
